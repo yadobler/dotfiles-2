827ce467d307458f027172a0ab82e3c545d2bbfd
Yadobler <yuknavell.adobler.android@gmail.com>
2023-12-10 17:15:42 +0800

    ye it not working, the qt tray

--- .config/scripts/dbus_waiter.sh
+++ .config/scripts/dbus_waiter.sh
@@ -1,8 +1,7 @@
-#! /bin/bash
-while ! dbus-send --session --dest=org.freedesktop.DBus --type=method_call --print-reply /org/freedesktop/DBus org.freedesktop.DBus.ListNames | grep org.kde.StatusNotifierWatcher; 
-do sleep 0.1; 
-done
-
+#! /bin/zsh
+# while ! dbus-send --session --dest=org.freedesktop.DBus --type=method_call --print-reply /org/freedesktop/DBus org.freedesktop.DBus.ListNames | grep org.kde.StatusNotifierWatcher; 
+# do sleep 0.1; 
+# done
+sleep 5
 telegram-desktop -autostart &
 disown
-exit
