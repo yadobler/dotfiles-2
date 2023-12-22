d6b82927dec94a8c40fddad90544a7c0a1b5e40b
Yadobler <yuknavell.adobler.android@gmail.com>
2023-12-23 03:54:59 +0800

    squeekboard

--- .config/waybar/custom_modules/open_kb.sh
+++ .config/waybar/custom_modules/open_kb.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/sh
 
-/bin/kill -SIGRTMIN+0 wvkbd-mobintl
+VISIBILITY=$(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible)
+
+if [[ $VISIBILITY == "b false" ]] then
+    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
+else
+    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
+fi
