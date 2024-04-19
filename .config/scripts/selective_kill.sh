32fd13cd427c2568252f85caf55ab2357e35a0ec
Yadobler <49450747+Yadobler@users.noreply.github.com>
2024-04-19 14:19:07 +0800

    switch shortcut to ncspot

--- .config/scripts/selective_kill.sh
+++ .config/scripts/selective_kill.sh
@@ -1,6 +1,6 @@
 #! /bin/zsh
 
-if [[ "$(hyprctl activewindow -j | jq -r ".class")" =~ "^(spt|Steam|brave-keep.google.com.+|brave-to-do.live.com.+|brave-open.spotify.com.+|brave-web.whatsapp.com.+|brave-mail.google.com__mail_u_.-Default|brave-calendar.google.com__calendar_u_1_r-Default)$" ]]; then
+if [[ "$(hyprctl activewindow -j | jq -r ".class")" =~ "^(ncspot|Steam|brave-keep.google.com.+|brave-to-do.live.com.+|brave-open.spotify.com.+|brave-web.whatsapp.com.+|brave-mail.google.com__mail_u_.-Default|brave-calendar.google.com__calendar_u_1_r-Default)$" ]]; then
     hyprctl dispatch movetoworkspacesilent special:$SCRATCHPAD_NAME
 else
     hyprctl dispatch killactive ""
