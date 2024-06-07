2d33d371b38db6e7cfae4e1de6ae14d7c23ef22e
Yadobler <49450747+Yadobler@users.noreply.github.com>
2024-06-08 03:28:05 +0800

    second try hyprland nix

--- .config/nixos/modules/hyprland.nix
+++ .config/nixos/modules/hyprland.nix
@@ -9,80 +9,78 @@
             url = "github:hyprwm/hyprland-plugins";
             inputs.hyprland.follows = "hyprland";
         };
-        iio-hyprland = { 
-            url = "github:JeanSchoeller/iio-hyprland";
-        };
         Hyprspace = {
             url = "github:KZDKM/Hyprspace";
             inputs.hyprland.follows = "hyprland";
         };
+        iio-hyprland = { 
+            url = "github:JeanSchoeller/iio-hyprland";
+        };
     };
     programs = {
-
         hyprland = {
             enable = true;
             package = inputs.hyprland.packages.${pkgs.system}.hyprland;
             xwayland.enable = true;
-             plugins = [
+            plugins = [
                 inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
                 inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
-             ];
-# portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland.override
-# {
-#     inherit (pkgs) mesa;
-# };
+            ];
+            portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland.override
+            {
+                inherit (pkgs) mesa;
+            };
         };
 
         hyprlock.enable = true;
-# programs.dconf.enable = true;
+        dconf.enable = true;
 
     };
     environment = { 
         sessionVariables = {
             NIXOS_OZONE_WL = "1"; # hint electron apps to use wayland
-            MOZ_ENABLE_WAYLAND = "1"; # ensure enable wayland for Firefox
-            WLR_RENDERER_ALLOW_SOFTWARE = "1"; # enable software rendering for wlroots
-            WLR_NO_HARDWARE_CURSORS = "1"; # disable hardware cursors for wlroots
-            NIXOS_XDG_OPEN_USE_PORTAL = "1"; # needed to open apps after web login
+                MOZ_ENABLE_WAYLAND = "1"; # ensure enable wayland for Firefox
+                WLR_RENDERER_ALLOW_SOFTWARE = "1"; # enable software rendering for wlroots
+                WLR_NO_HARDWARE_CURSORS = "1"; # disable hardware cursors for wlroots
+                NIXOS_XDG_OPEN_USE_PORTAL = "1"; # needed to open apps after web login
         };
         systemPackages = with pkgs; [
             inputs.iio-hyprland.packages.${pkgs.system}.default
             # hyprcursor
         ];
-    }
+    };
 
     services = {
         hypridle.enable = true;
-# dbus = {
-#     enable = true;
-#     packages = with pkgs; [
-#         gcr 
-#         dconf 
-#     ];
-# };
+        dbus = {
+            enable = true;
+            packages = with pkgs; [
+                gcr
+                    dconf
+            ];
+        };
     };
-
-
-# xdg = {
-#     portal = {
-#         enable = true;
-#         extraPortals = with pkgs; [
-#             xdg-desktop-portal-gtk
-#         ];
-#         configPackages = [
-#             pkgs.xdg-desktop-portal-gtk
-#         ];
-#     };
-# };
 
     hardware = {
         opengl =
-         {
-             enable = true;
-             driSupport = true;
-             driSupport32Bit = true;
-# package = pkgs.mesa.drivers;
-# package32 = pkgs.pkgsi686Linux.mesa.drivers;
+        {
+            enable = true;
+            driSupport = true;
+            driSupport32Bit = true;
+            package = pkgs.mesa.drivers;
+            package32 = pkgs.pkgsi686Linux.mesa.drivers;
+        };
+    };
+
+    xdg = {
+        portal = {
+            enable = true;
+            extraPortals = with pkgs; [
+                xdg-desktop-portal-gtk
+            ];
+            configPackages = [
+                pkgs.xdg-desktop-portal-gtk
+            ];
         };
     };
 }
