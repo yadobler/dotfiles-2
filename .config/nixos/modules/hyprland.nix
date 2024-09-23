5d071715c92dbbfe5d1910a4820771b272dea1a9
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-09-23 12:18:22 +0800

    fix brave download issue 3

--- .config/nixos/modules/hyprland.nix
+++ .config/nixos/modules/hyprland.nix
@@ -75,13 +75,12 @@ in
         xdg = {
             portal = {
                 enable = true;
+                wlr.enable = true;
                 extraPortals = with pkgs; [
-                    xdg-desktop-portal-gtk
+                    xdg-desktop-portal-hyprland
                 ];
-                configPackages = [
-                    # pkgs.xdg-desktop-portal-gtk
-                    # pkgs.xdg-desktop-portal-wlr
-                    pkgs.xdg-desktop-portal-hyprland
+                configPackages = with pkgs; [
+                    xdg-desktop-portal-hyprland
                 ];
             };
         };
