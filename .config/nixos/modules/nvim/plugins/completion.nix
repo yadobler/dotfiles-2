afb91ec8200489f8950b657aac686b12f920a285
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-12-23 23:30:09 +0800

    Add more to completion

--- .config/nixos/modules/nvim/plugins/completion.nix
+++ .config/nixos/modules/nvim/plugins/completion.nix
@@ -7,6 +7,7 @@
             cmp-spell.enable = true;
             cmp-emoji.enable = true;
             cmp-buffer.enable = true;
+            cmp-cmdline.enable = true;
             cmp-path.enable = true;
             cmp-nvim-lsp.enable = true;
             cmp-nvim-lsp-signature-help.enable = true;
@@ -15,60 +16,80 @@
             cmp = {
                 enable = true;
                 autoEnableSources = true;
-                cmdline = {
-                    "/" = {
-                        mapping.__raw = "cmp.mapping.preset.cmdline()";
-                        sources = [ { name = "buffer"; } ];
-                    };
-
-                    ":" = {
-                        mapping.__raw = "cmp.mapping.preset.cmdline()";
-                        sources = [
-                        { name = "path"; }
-                        {
-                            name = "cmdline";
-                            option.ignore_cmds = [
-                                "Man"
-                                    "!"
-                            ];
-                        }
-                        ];
-                    };
-                };
 
                 filetype = {};
-
                 settings = {
-# Preselect first entry
+                    # Preselect first entry
                     completion.completeopt = "menu,menuone,noinsert";
                     sources = [
-                    {
-                        name = "nvim_lsp";
-                        priority = 100;
-                    }
-                    {
-                        name = "nvim_lsp_signature_help";
-                        priority = 100;
-                    }
-                    {
-                        name = "nvim_lsp_document_symbol";
-                        priority = 100;
-                    }
-                    {
-                        name = "treesitter";
-                        priority = 80;
-                    }
-                    {
-                        name = "luasnip";
-                        priority = 70;
-                    }
+                        { name = "nvim_lsp"; priority = 100; }
+                        { name = "nvim_lsp_signature_help"; priority = 100; }
+                        { name = "nvim_lsp_document_symbol"; priority = 100; }
+                        { name = "treesitter"; priority = 80; }
+                        { name = "luasnip"; priority = 70; }
+                        { name = "path"; priority = 25; }
+                    ];
+
+                    window = {
+                        completion.border = "single";
+                        documentation.border = "single";
+                    };
+
+                    performance = {
+                        debounce = 60;
+                        fetching_timeout = 200;
+                        max_view_entries = 30;
+                    };
+
+                    mapping = {
+                        "<Up>"      = "cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }";
+                        "<Down>"    = "cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }";
+                        "<C-p>"     = "cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }";
+                        "<C-n>"     = "cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }";
+                        "<C-k>"     = "cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }";
+                        "<C-j>"     = "cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }";
+                        "<C-u>"     = "cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' })";
+                        "<C-d>"     = "cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' })";
+                        "<C-Space>" = "cmp.mapping(cmp.mapping.complete(), { 'i', 'c' })";
+                        "<C-e>"     = "cmp.mapping.abort()";
+                        "<CR>"      = "cmp.mapping.confirm({ select = false })";
+                        "<Tab>"     = ''
+                            cmp.mapping(
+                                    function(fallback)
+                                        local cmp = require('cmp')
+                                        local luasnip = require("luasnip")
+                                        local has_words_before = function()
+                                            unpack = unpack or table.unpack
+                                            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                                            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil                                                                                         
+                                        end
+                                        if cmp.visible() then
+                                            cmp.select_next_item()
+                                        elseif luasnip.expand_or_jumpable() then
+                                            luasnip.expand_or_jump()
+                                        elseif has_words_before() then
+                                            cmp.complete()
+                                        else
+                                            fallback()
+                                        end
+                                    end, { "i", "s" })
+                            '';
+                    };
                 };
-
-                extraPlugins = with pkgs.vimPlugins; [
+            };
+            
+            luasnip = {
+                enable = true;
+                settings = {
+                    enable_autosnippets = true;
+                    store_selection_keys = "<Tab>";
+                };
+                fromVscode = [
+                {
+                    lazyLoad = true;
+                    paths = "${pkgs.vimPlugins.friendly-snippets}";
+                }
                 ];
-
-                extraConfigLua = ''
-                    '';
             };
 
             lspkind = {
@@ -136,4 +157,6 @@
                     ellipsis_char = "...";
                 };
             };
-        }
+        };
+    };
+}
