014651ffe4e584e8ff058636d6ac663ec08a4243
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-12-25 23:22:55 +0800

    Fix nesting

--- .config/nixos/modules/nvim/plugins/lspsaga.nix
+++ .config/nixos/modules/nvim/plugins/lspsaga.nix
@@ -58,22 +58,23 @@
                     scrollDown = "<C-u>";
                 };
             };
-
-            which-key.settings.spec = [
-                {
-                    __unkeyed-1 = "gp";
-                    mode = "n";
-                    group = "+peek";
-                }
-            ];
         };
 
-        keymaps = [
+        which-key.settings.spec = [
             {
+                __unkeyed-1 = "gp";
                 mode = "n";
-                key = "K";
-                # action = "<cmd>Lspsaga hover_doc<CR>";
-                action.__raw = ''
+                group = "+peek";
+            }
+        ];
+    };
+
+    keymaps = [
+        {
+            mode = "n";
+            key = "K";
+            # action = "<cmd>Lspsaga hover_doc<CR>";
+            action.__raw = ''
         function()
             local winid = require("ufo").peekFoldedLinesUnderCursor()
             if not winid then
@@ -81,117 +82,115 @@
             end
         end
         '';
-                options = {
-                    desc = "Hover";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "<leader>lo";
-                action = "<cmd>Lspsaga outline<CR>";
-                options = {
-                    desc = "Outline";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "<leader>lr";
-                action = "<cmd>Lspsaga rename<CR>";
-                options = {
-                    desc = "Rename";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "<leader>ca";
-                action = "<cmd>Lspsaga code_action<CR>";
-                options = {
-                    desc = "Code Action";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "<leader>cd";
-                action = "<cmd>Lspsaga show_buf_diagnostics<CR>";
-                options = {
-                    desc = "Line Diagnostics";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "gd";
-                action = "<cmd>Lspsaga goto_definition<CR>";
-                options = {
-                    desc = "Goto Definition";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "gt";
-                action = "<cmd>Lspsaga goto_type_definition<CR>";
-                options = {
-                    desc = "Type Definitions";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "gpd";
-                action = "<cmd>Lspsaga peek_definition<CR>";
-                options = {
-                    desc = "Peek Definitions";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "gpt";
-                action = "<cmd>Lspsaga peek_type_definition<CR>";
-                options = {
-                    desc = "Peek Type Definitions";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "gl";
-                action = "<cmd>Lspsaga show_line_diagnostics<CR>";
-                options = {
-                    desc = "Line Diagnostics";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "[d";
-                action = "<cmd>Lspsaga diagnostic_jump_next<CR>";
-                options = {
-                    desc = "Next Diagnostic";
-                    silent = true;
-                };
-            }
-            {
-                mode = "n";
-                key = "]d";
-                action = "<cmd>Lspsaga diagnostic_jump_prev<CR>";
-                options = {
-                    desc = "Previous Diagnostic";
-                    silent = true;
-                };
-            }
-        ];
-    };
+            options = {
+                desc = "Hover";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "<leader>lo";
+            action = "<cmd>Lspsaga outline<CR>";
+            options = {
+                desc = "Outline";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "<leader>lr";
+            action = "<cmd>Lspsaga rename<CR>";
+            options = {
+                desc = "Rename";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "<leader>ca";
+            action = "<cmd>Lspsaga code_action<CR>";
+            options = {
+                desc = "Code Action";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "<leader>cd";
+            action = "<cmd>Lspsaga show_buf_diagnostics<CR>";
+            options = {
+                desc = "Line Diagnostics";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "gd";
+            action = "<cmd>Lspsaga goto_definition<CR>";
+            options = {
+                desc = "Goto Definition";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "gt";
+            action = "<cmd>Lspsaga goto_type_definition<CR>";
+            options = {
+                desc = "Type Definitions";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "gpd";
+            action = "<cmd>Lspsaga peek_definition<CR>";
+            options = {
+                desc = "Peek Definitions";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "gpt";
+            action = "<cmd>Lspsaga peek_type_definition<CR>";
+            options = {
+                desc = "Peek Type Definitions";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "gl";
+            action = "<cmd>Lspsaga show_line_diagnostics<CR>";
+            options = {
+                desc = "Line Diagnostics";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "[d";
+            action = "<cmd>Lspsaga diagnostic_jump_next<CR>";
+            options = {
+                desc = "Next Diagnostic";
+                silent = true;
+            };
+        }
+        {
+            mode = "n";
+            key = "]d";
+            action = "<cmd>Lspsaga diagnostic_jump_prev<CR>";
+            options = {
+                desc = "Previous Diagnostic";
+                silent = true;
+            };
+        }
+    ];
 
     extraPlugins = with pkgs.vimPlugins; [
     ];
 
     extraConfigLua = ''
     '';
-};
-    }
+}
