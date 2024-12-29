c8e8e680507e94badebf924a6bb78638490ed8f6
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-12-30 05:12:14 +0800

    Add gitsigns

--- .config/nixos/modules/nvim/plugins/theme/default.nix
+++ .config/nixos/modules/nvim/plugins/theme/default.nix
@@ -5,14 +5,4 @@
       enable = true;
     };
   };
-
-  # extraPlugins = with pkgs.vimPlugins; [
-  #         gruvbox-baby
-  #     ];
-
-  #     extraConfigLua = ''
-  #             vim.g.gruvbox_baby_telescope_theme = 1
-  #             vim.g.gruvbox_baby_background_color = "dark"
-  #             vim.cmd[[colorscheme gruvbox-baby]]
-  #     '';
 }
