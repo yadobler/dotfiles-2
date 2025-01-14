5e4edddbc631f1ebc8f384197603d94565f32234
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-14 13:48:21 +0800

    Fix volume script

--- .config/scripts/volume.sh
+++ .config/scripts/volume.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env zsh
+
 VOLUME=$(wpctl get-volume @DEFAULT_SINK@ | awk '{print $2}')
 notification_id="/tmp/VOLUME_NOTIFICATION_ID"
 [[ ! -a $notification_id ]] && echo 1 > $notification_id
