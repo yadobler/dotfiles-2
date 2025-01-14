e6eb29b29e3771a0e4a878a0fecb1656ee22d3f7
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-14 14:48:53 +0800

    Change window border colors

--- .config/scripts/launch_one_instance.sh
+++ .config/scripts/launch_one_instance.sh
@@ -1,9 +1,9 @@
-#/usr/bin/env bash
 ADDRESS=$(hyprctl clients -j | jq -r ".[] | select(.class==\"$1\") | .address")
 ACTIVE_WORKSPACE=$(hyprctl activeworkspace -j | jq -r '.["id"]')
-echo $ADDRESS $ACTIVE_WORKSPACE
-[[ $(echo $ADDRESS | wc --lines) -le 1 ]] && echo new || echo switch
-[[ $(echo $ADDRESS | wc --lines) -le 1 ]] && $2 || hyprctl --batch "\
+echo "$ADDRESS" "$ACTIVE_WORKSPACE"
+exit
+[[ $(echo "$ADDRESS" | wc --lines) -le 1 ]] && echo new || echo switch
+[[ $(echo "$ADDRESS" | wc --lines) -le 1 ]] && $1 || hyprctl --batch "\
         dispatch movetoworkspacesilent $ACTIVE_WORKSPACE,address:$ADDRESS;\
         dispatch focuswindow address:$ADDRESS;\
         dispatch centerwindow"
