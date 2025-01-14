4b4fae348b6dbf398562c825b694fdbea0fa97cf
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-14 14:59:47 +0800

    Change themes and try fish

--- .config/nixos/modules/terminal.nix
+++ .config/nixos/modules/terminal.nix
@@ -1,10 +1,11 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, username, ... }:
 let
   terminal = pkgs.foot;
   binary_name = "foot";
-  shell = pkgs.zsh;
+  #shell = pkgs.zsh;
+  shell = pkgs.fish;
 in
-{
+  {
   config = {
     programs = {
       zsh = {
@@ -32,51 +33,94 @@ in
         ];
         shellAliases = {
           vim = "nvim";
-            "svim" = "sudo -E nvim";
-            "batt" = "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -e state -e percentage -e time\ to\ empty";
-            "ip" = "ip -color=auto";
-            "ls" = "lsd --group-directories-first";
-            "la" = "ls -lA";
-            "ll" = "la -hN";
-            "cat" = "bat";
-            "du" = "dust";
-            "ps" = "procs";
-            "htop" = "btm";
-            "grep" = "rg";
-            "find" = "fd";
-            "jobs" = "jobs -p";
+          "svim" = "sudo -E nvim";
+          "batt" = "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -e state -e percentage -e time\ to\ empty";
+          "ip" = "ip -color=auto";
+          "ls" = "lsd --group-directories-first";
+          "la" = "ls -lA";
+          "ll" = "la -hN";
+          "cat" = "bat";
+          "du" = "dust";
+          "ps" = "procs";
+          "htop" = "btm";
+          "grep" = "rg";
+          "find" = "fd";
+          "jobs" = "jobs -p";
 
-            "wal_update" = "~/.config/scripts/wallust_update.sh";
-            "steam_update_apps" = "sed 's/Exec=steam /Exec=gamemoderun steam /g' -i ~/.local/share/applicationsCC/*";
-            "valgrind" = "~/.config/scripts/colorgrind";
-            "footserver" = "foot --server &; disown";
-            "cd.." = "cd ..";
-            ":q" = "exit";
+          "wal_update" = "~/.config/scripts/wallust_update.sh";
+          "steam_update_apps" = "sed 's/Exec=steam /Exec=gamemoderun steam /g' -i ~/.local/share/applicationsCC/*";
+          "valgrind" = "~/.config/scripts/colorgrind";
+          "footserver" = "foot --server &; disown";
+          "cd.." = "cd ..";
+          ":q" = "exit";
 
-            "gaf" = "git add -f";
-            "gau" = "git add -u";
-            "gcm" = "git commit -m";
-            "gc" = "git commit -m";
-            "gm" = "git merge";
-            "gm-noff" = "git merge --no-ff";
-            "gp" = "git push";
-            "gpo" = "git push -u origin $(git branch --show-current)";
-            "gptags" = "git push --tags";
-            "gpull" = "git pull";
-            "gs" = "git status";
-            "gl" = "git log";
-            "gswitch" = "git switch";
-            "gswitchc" = "git switch -c";
-            "gsm" = "git submodule";
-            "gtag" = "git tag";
+          "gaf" = "git add -f";
+          "gau" = "git add -u";
+          "gcm" = "git commit -m";
+          "gc" = "git commit -m";
+          "gm" = "git merge";
+          "gm-noff" = "git merge --no-ff";
+          "gp" = "git push";
+          "gpo" = "git push -u origin $(git branch --show-current)";
+          "gptags" = "git push --tags";
+          "gpull" = "git pull";
+          "gs" = "git status";
+          "gl" = "git log";
+          "gswitch" = "git switch";
+          "gswitchc" = "git switch -c";
+          "gsm" = "git submodule";
+          "gtag" = "git tag";
+        };
+      };
+      fish = {
+        enable = true;
+        shellAbbrs  = {
+          "vim" = "nvim";
+          "svim" = "sudo -E nvim";
+          "batt" = "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -e state -e percentage -e time\ to\ empty";
+          "ip" = "ip -color=auto";
+          "ls" = "lsd --group-directories-first";
+          "la" = "ls -lA";
+          "ll" = "la -hN";
+          "cat" = "bat";
+          "du" = "dust";
+          "ps" = "procs";
+          "htop" = "btm";
+          "grep" = "rg";
+          "find" = "fd";
+          "jobs" = "jobs -p";
+
+          "wal_update" = "~/.config/scripts/wallust_update.sh";
+          "steam_update_apps" = "sed 's/Exec=steam /Exec=gamemoderun steam /g' -i ~/.local/share/applicationsCC/*";
+          "valgrind" = "~/.config/scripts/colorgrind";
+          "footserver" = "foot --server &; disown";
+          "cd.." = "cd ..";
+          ":q" = "exit";
+
+          "gaf" = "git add -f";
+          "gau" = "git add -u";
+          "gcm" = "git commit -m";
+          "gc" = "git commit -m";
+          "gm" = "git merge";
+          "gm-noff" = "git merge --no-ff";
+          "gp" = "git push";
+          "gpo" = "git push -u origin $(git branch --show-current)";
+          "gptags" = "git push --tags";
+          "gpull" = "git pull";
+          "gs" = "git status";
+          "gl" = "git log";
+          "gswitch" = "git switch";
+          "gswitchc" = "git switch -c";
+          "gsm" = "git submodule";
+          "gtag" = "git tag";
+
         };
       };
     };
-    
-    users.users.yukna.shell = shell;
+
+    users.users.${username}.shell = shell;
     environment.systemPackages = [
       terminal
-
     ];
 
     system.activationScripts.postInstall = ''
