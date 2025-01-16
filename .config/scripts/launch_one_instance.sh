c00edc5e57925e1ed7778fe83eee2d9541ab7a4c
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-16 14:35:35 +0800

    Fix one_instance launching

--- .config/scripts/launch_one_instance.sh
+++ .config/scripts/launch_one_instance.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 ADDRESS=$(hyprctl clients -j | jq -r ".[] | select(.class==\"$1\") | .address")
 ACTIVE_WORKSPACE=$(hyprctl activeworkspace -j | jq -r '.["id"]')
-echo "$ADDRESS" "$ACTIVE_WORKSPACE"
+# echo $([[ -z "$ADDRESS" ]] && echo yes) "$ACTIVE_WORKSPACE"
 
-[[ $(echo "$ADDRESS" | wc --lines) -le 1 ]] && echo new || echo switch
-[[ $(echo "$ADDRESS" | wc --lines) -le 1 ]] && $2 || hyprctl --batch "\
+if [[ -z "$ADDRESS" ]]; then 
+    $2 
+else 
+    hyprctl --batch "\
         dispatch movetoworkspacesilent $ACTIVE_WORKSPACE,address:$ADDRESS;\
         dispatch focuswindow address:$ADDRESS;\
         dispatch centerwindow"
+fi
