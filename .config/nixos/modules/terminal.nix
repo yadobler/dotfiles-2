85cf749a55845c1a00b8db29d52c2e933b5686b3
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-18 22:17:43 +0800

    Change fish colors

--- .config/nixos/modules/terminal.nix
+++ .config/nixos/modules/terminal.nix
@@ -31,7 +31,7 @@ in
       '';
       interactiveShellInit = ''
             fish_vi_key_bindings
-            tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
+            tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
       '';
 
       shellAbbrs  = {
