4ea29c9e75436eb46bfa6711ee3e1fd6bc7e6960
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-28 02:40:35 +0800

    Fix volume script to use bash

--- .config/scripts/volume.sh
+++ .config/scripts/volume.sh
@@ -25,19 +25,17 @@ if [[ $(wpctl get-volume @DEFAULT_SINK@ | awk '{print $3}') == '[MUTED]' ]]; the
         -a "volume_notification_script" \
         -p > $notification_id
 else
-    # HACK: using zsh for FPA - if using sh, change these lines
-    zmodload zsh/mathfunc
-    VOLUME=$(( int(100 * float($(wpctl get-volume @DEFAULT_SINK@ | awk '{print $2}'))) ))
+    VOLUME=$(wpctl get-volume @DEFAULT_SINK@ | awk '{print 100 * $2}')
     VOLUME_ICON="notification-audio-volume-medium"
-    if [ $VOLUME -le 30 ]; then VOLUME_ICON="notification-audio-volume-low"; fi
-    if [ $VOLUME -ge 75 ]; then VOLUME_ICON="notification-audio-volume-high"; fi
-    if [ $VOLUME -le 1 ]; then VOLUME_ICON="notification-audio-volume-muted"; fi
+    if [ "$VOLUME" -le 30 ]; then VOLUME_ICON="notification-audio-volume-low"; fi
+    if [ "$VOLUME" -ge 75 ]; then VOLUME_ICON="notification-audio-volume-high"; fi
+    if [ "$VOLUME" -le 1 ]; then VOLUME_ICON="notification-audio-volume-muted"; fi
     notify-send \
         "Volume: $VOLUME%"  \
         -r $(<$notification_id) \
         -t 1000 \
         -i $VOLUME_ICON \
-        -h int:value:$VOLUME \
+        -h int:value:"$VOLUME" \
         -h string:synchronous:volume-change \
         -a "volume_notification_script"\
         -p > $notification_id
