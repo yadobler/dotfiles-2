16a8c53591c1ca3cff06717e7c29f0bebde07006
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-02-09 06:06:11 +0800

    Finish updates

--- .config/scripts/hyprland-plugin-script_2.sh
+++ .config/scripts/hyprland-plugin-script_2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env /bin/sh
-hyprctl plugins load /nix/store/a5fxab607wys0bqagxj23r9z103a4v4d-hyprexpo-0.46.0/lib/*.so
-hyprctl plugins load /nix/store/qzlk3yfj413qdjlnscpahdlhs0mib8xj-hyprspace-0-unstable-2024-12-08/lib/*.so
-hyprctl plugins load /nix/store/5pz8yqwbp9390jis88w25rf2pzq8vwq3-hyprgrass-0.8.2-unstable-2024-12-13/lib/*.so
+hyprctl plugins load /nix/store/31jjjyay0ja6zdhmzq48f7j15m3jm1iw-hyprexpo-0.47.0/lib/*.so
+hyprctl plugins load /nix/store/qdv2rkbgikf6b2qwdy3mrykm4acyva1w-hyprspace-0-unstable-2024-11-02/lib/*.so
+hyprctl plugins load /nix/store/z5711fp18j15gg94wsva8pfqxjv8m6f5-hyprgrass-0.8.2-unstable-2025-02-01/lib/*.so
