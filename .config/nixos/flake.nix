5add34dee0d1e90cc2b88e098df411eefee42596
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-02-09 05:19:37 +0800

    Update stable source

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "An example NixOS configuration";
   inputs = {
     nixpkgs-stable = {
-      url = "github:NixOS/nixpkgs/nixos-24.05";
+      url = "github:NixOS/nixpkgs/nixos-24.11";
     };
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -16,15 +16,18 @@
 
   outputs = { self, nixpkgs, nixpkgs-stable, ...} @inputs:
     let
+
       system = "x86_64-linux";
       username = "yukna";
       specialArgs = { inherit inputs; inherit system; inherit username; };
+
       overlay-stable = final: prev: {
         stable = import inputs.nixpkgs-stable {
           inherit system;
           config.allowUnfree = true;
         };
       };
+
     in {
       nixosConfigurations.vellinator = nixpkgs.lib.nixosSystem {
         inherit system;
