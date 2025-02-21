53a7bff7932a03ec1ba0ef649ad5085a1be75a4e
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-02-21 19:14:30 +0800

    Add gtk theming

--- .config/nixos/modules/theme.nix
+++ .config/nixos/modules/theme.nix
@@ -1,5 +1,13 @@
-{ pkgs, ...}:
-  let colorscheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml"; in
-{
+{ pkgs, username,...}:
+let 
+  colorscheme_name = "oxocarbon-dark";
+  colorscheme = "${pkgs.base16-schemes}/share/themes/${colorscheme_name}.yaml"; 
+in {
   environment.systemPackages = with pkgs; [ flavours ];
+  system.userActivationScripts.check_colorscheme = ''
+    [ "$(flavours current)" == "${colorscheme_name}" ] && exit
+    mkdir -p /home/${username}/.config/flavours/schemes/${colorscheme_name}
+    cp -u ${colorscheme} /home/${username}/.config/flavours/schemes/${colorscheme_name}/${colorscheme_name}.yaml
+    flavours -c /home/${username}/.config/flavours/config.toml -d /home/${username}/.config/flavours/ apply ${colorscheme_name}
+  '';
 }
