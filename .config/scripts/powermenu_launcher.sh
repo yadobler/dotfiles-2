0b4388c5da3e653793dd70e19c5dcc18f82c6c1f
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-02-26 04:11:26 +0800

    Add sounds to everything lol

--- .config/scripts/powermenu_launcher.sh
+++ .config/scripts/powermenu_launcher.sh
@@ -4,18 +4,22 @@ show_powermenu() {
     choice=$(printf "󰤄 Hibernate\n⏼ Shutdown\n Reboot\n Lock" | wofi --style ~/.config/wofi/style.css --dmenu -i --height 320 -O default)
     case ${choice:2} in
         Hibernate)
+            pw-play ~/.config/scripts/assets/winxpshutdown.wav &
             hyprlock &
             disown
             sleep 2
             systemctl hibernate
             ;;
         Shutdown)
+            pw-play ~/.config/scripts/assets/winxpshutdown.wav &
             shutdown now
             ;;
         Reboot)
+            pw-play ~/.config/scripts/assets/winxpshutdown.wav &
             systemctl reboot
             ;;
         Lock)
+            pw-play ~/.config/scripts/assets/winxpshutdown.wav &
             hyprlock &
             disown
             ;;
