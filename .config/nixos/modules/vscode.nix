3f0de8307f4558c31bf3526190b452a06a64bf4d
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-03-08 10:06:47 +0800

    Use vscodium-vhs

--- .config/nixos/modules/vscode.nix
+++ .config/nixos/modules/vscode.nix
@@ -1,36 +1,37 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        sainnhe.gruvbox-material
-        emroussel.atomize-atom-one-dark-theme
-
-        asvetliakov.vscode-neovim
-        
-        ms-python.python
-        ms-python.pylint
-        ms-python.debugpy
-        ms-toolsai.jupyter
-        ms-toolsai.jupyter-renderers
-
-        astro-build.astro-vscode
-        bradlc.vscode-tailwindcss
-        esbenp.prettier-vscode
-        jnoortheen.nix-ide
-        pkief.material-icon-theme
-        gleam.gleam 
-
-
-
-      ]++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "ProbeJS";
-            publisher = "Prunoideae";
-            version = "0.4.2";
-            sha256 = "+/qcGtrjsStTyfqF9G11wR+FsFCSQDf067pAYjOIdaI=";
-          }
-        ];
-    })
+    vscodium-fhs
+    # (vscode-with-extensions.override {
+    #   vscodeExtensions = with vscode-extensions; [
+    #     sainnhe.gruvbox-material
+    #     emroussel.atomize-atom-one-dark-theme
+    #
+    #     asvetliakov.vscode-neovim
+    #
+    #     ms-python.python
+    #     ms-python.pylint
+    #     ms-python.debugpy
+    #     ms-toolsai.jupyter
+    #     ms-toolsai.jupyter-renderers
+    #
+    #     astro-build.astro-vscode
+    #     bradlc.vscode-tailwindcss
+    #     esbenp.prettier-vscode
+    #     jnoortheen.nix-ide
+    #     pkief.material-icon-theme
+    #     gleam.gleam 
+    #
+    #
+    #
+    #   ]++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    #       {
+    #         name = "ProbeJS";
+    #         publisher = "Prunoideae";
+    #         version = "0.4.2";
+    #         sha256 = "+/qcGtrjsStTyfqF9G11wR+FsFCSQDf067pAYjOIdaI=";
+    #       }
+    #     ];
+    # })
   ];
 }
