a4c2014185ca5fac4efc20f96317028896abe2ea
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-04-01 20:46:11 +0800

    Add qemu options

--- .config/nixos/modules/qemu.nix
+++ .config/nixos/modules/qemu.nix
@@ -8,11 +8,19 @@
   virtualisation = {
     libvirtd = {
       enable = true;
-      qemu.vhostUserPackages = [ pkgs.virtiofsd ];
+      qemu = {
+        vhostUserPackages = [ pkgs.virtiofsd ];
+        options = [
+          "-vga qxl"
+          "-spice port=5924,disable-ticketing=on"
+          "-device virtio-serial -chardev spicevmc,id=vdagent,debug=0,name=vdagent"
+          "-device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
+        ];
+      };
     };
     spiceUSBRedirection.enable = true;
   };
 
   services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true;  # enable copy and paste between host and guest
+  services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
 }
