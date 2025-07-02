81ff7bb0896ef351b3fe06430e2ab47b47d05694
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-07-02 18:03:18 +0800

    Update waybar

--- .config/waybar/custom_modules/open_kb.sh
+++ .config/waybar/custom_modules/open_kb.sh
@@ -1,15 +1,18 @@
-#! /usr/bin/env zsh
-if [[ ! $(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible) ]] then
-    squeekboard &
-    disown
-    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
-    exit
-fi
+#!/usr/bin/env sh
 
-VISIBILITY=$(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible)
+killall wkeys || wkeys
 
-if [[ $VISIBILITY == "b false" ]] then
-    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
-else
-    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
-fi
+# if [[ ! $(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible) ]] then
+#     squeekboard &
+#     disown
+#     busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
+#     exit
+# fi
+#
+# VISIBILITY=$(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible)
+#
+# if [[ $VISIBILITY == "b false" ]] then
+#     busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
+# else
+#     busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
+# fi
