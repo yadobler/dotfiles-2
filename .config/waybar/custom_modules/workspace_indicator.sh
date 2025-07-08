69e517f28067a66dd33b0a5c158786ec3577859a
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-07-08 19:39:42 +0800

    Update niri workspace indicator

--- .config/waybar/custom_modules/workspace_indicator.sh
+++ .config/waybar/custom_modules/workspace_indicator.sh
@@ -7,7 +7,6 @@ set -e
 WINDOWS_JSON=$(niri msg -j windows)
 WORKSPACES_JSON=$(niri msg -j workspaces)
 
-# Use jq to process the data and output a single JSON object for Waybar
 jq -nc --argjson windows "$WINDOWS_JSON" --argjson workspaces "$WORKSPACES_JSON" '
   # --- This first part for finding icons is the same ---
   ($workspaces | map(select(.is_focused)) | .[0].id) as $focused_ws_id |
@@ -17,34 +16,35 @@ jq -nc --argjson windows "$WINDOWS_JSON" --argjson workspaces "$WORKSPACES_JSON"
     map(.app_id |
         if contains("brave") or contains("firefox") then "󰖟"
         elif contains("ghostty") or contains("kitty") or contains("alacritty") then ""
-        elif contains("thunar") or contains("nautilus") then ""
-        elif contains("telegram") then ""
-        elif contains("brave-web.whatsapp.com") then ""
+        elif contains("thunar") or contains("nautilus") then "<span foreground=\"light yellow\"></span>"
+        elif contains("telegram") then "<span foreground=\"light blue\"></span>"
+        elif contains("brave-web.whatsapp.com__-Default") then "<span foreground=\"green\"></span>"
         elif contains("brave-calendar.google.com") then ""
         elif contains("brave-mail.google.com") then ""
         elif contains("brave-to-do.live.com") then ""
-        elif contains("spotify") or contains("spt") or contains("Spotify Premium") then ""
+        elif contains("spotify") or contains("spt") or contains("Spotify Premium") then "<span foreground=\"green\"></span>"
         else "" # Default window icon
         end
-    ) | if length > 0 then " " + join(" ") else "" end
+    ) | 
+    map(.) | if length > 0 then " " + join(" ") else "" end
   ) as $window_icons |
 
   # --- This final part is modified to create the desired output ---
   (
     $workspaces |
     sort_by(.idx) |
+    .[0:-1] | # omit last empty workspace
     map(
       .name = (if .name == null then (.idx | tostring) else .name end) |
       if .is_focused then
         # Use Pango markup to make the active workspace bold
-        .name = "<b>" + .name + $window_icons + "</b>"
+        .name = "<span foreground=\"light green\">[<b>" + .name + "</b></span>" + $window_icons + "<span foreground=\"light green\">]</span>"
       end
-    ) | map(.name) | join("  ")
+    ) | map(.name) | join("<span foreground=\"pink\"> | </span>") 
   ) as $final_text |
 
   # Construct the final JSON object for the custom Waybar module
   {
     "text": $final_text,
-    "class": "active"
   }
 '
