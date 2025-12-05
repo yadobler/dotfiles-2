bdc5ad8ffcf35fe467e4b4f9bbe42eb264f03315
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-12-06 01:12:38 +0800

    Add ll command alias

--- .config/nixos/modules/terminal.nix
+++ .config/nixos/modules/terminal.nix
@@ -108,6 +108,7 @@ in
         "batt"              = "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -e state -e percentage -e time\ to\ empty";
         "ip"                = "ip -color = auto";
         "ls"                = "eza --group-directories-first --icons=always";
+        "ll"                = "eza --group-directories-first -l";
         "la"                = "eza --group-directories-first -la";
         "l1"                = "eza --group-directories-first -1";
         "tree"              = "eza -T";
